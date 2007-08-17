1f394070d0ccf70a573a33481bdc27352fac07d9
schupp <schupp@9a5bccf9-763e-4bb7-9a1e-6a8f8440984e>
2007-08-17 18:07:25 +0000

    replace "ccmdb_foo ..." with "ccmdb foo ..."

--- lib/VCS/CMSynergy/Client.pm
+++ lib/VCS/CMSynergy/Client.pm
@@ -1,6 +1,6 @@
 package VCS::CMSynergy::Client;
 
-our $VERSION = do { (my $v = q%version: 12 %) =~ s/.*://; sprintf("%d.%02d", split(/\./, $v), 0) };
+our $VERSION = do { (my $v = q%version: 13 %) =~ s/.*://; sprintf("%d.%02d", split(/\./, $v), 0) };
 
 =head1 NAME
 
@@ -501,14 +501,14 @@ sub status
 
 
 # FIXME does not work on windows 
-# (also not on unix clients that don't have ccmdb_server installed)
+# (also not on unix clients that don't have the ccmdb program installed)
 sub databases	
 {
     my ($this, $servername) = @_;
     $this = __PACKAGE__->default unless ref $this;
 
     my @ccmdb_server = 
-	(File::Spec->catfile($this->ccm_home, qw(bin ccmdb_server)), '-status');
+	(File::Spec->catfile($this->ccm_home, qw(bin ccmdb)), qw/server -status/);
     push @ccmdb_server, $servername if defined $servername;
 
     my ($rc, $out, $err) = $this->exec(@ccmdb_server);
@@ -516,7 +516,7 @@ sub databases
 
     # strip leading/trailing stuff
     my ($list) = $out =~ /^===.*?$(.*?)^There is a total/ms;
-    return $this->set_error("unrecognized output from \"ccmdb_server -status\": $out")
+    return $this->set_error("unrecognized output from \"ccmdb server -status\": $out")
 	unless defined $list;
     return grep { !/dbpath not available/ }
            map  { (split(' ', $_, 3))[2]  } 

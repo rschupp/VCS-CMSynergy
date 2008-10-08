f65c8d7aab5444843bba3fa23569e471c9eee44e
rschupp <roderich.schupp@gmail.com>
2008-10-08 16:46:47 +0000

    Clean up and documentation _fullname2objectname(): drop special case for "releasedef": Synergy   (at least since 6.3) accepts objectnames with ":" (instead of   the delimiter) as first separator and our objectname_rx does it, too _rewrite_rule: document its structure (and give better names to its fields)

--- lib/VCS/CMSynergy.pm
+++ lib/VCS/CMSynergy.pm
@@ -507,17 +507,11 @@ sub _query
 # doesn't accept these where a "file_spec" is expected 
 # (at least on Unix, because they contain slashes). 
 # Hence rewrite these fullnames to objectnames.
-# Arrggh. Some moron implemented a bogus "objectname" acc_method 
-# for release objects (i.e. type "releasedef") that
-# emits ":" as the first separator. These objectnames aren't
-# accepted as file_specs either. Rewrite them, too.
 sub _fullname2objectname
 {
     my ($self, $fullname) = @_;
     $fullname =~ s{^(.*?)/(.*?)/(.*?)/(.*?)$}
 	          {$3$self->{delimiter}$4:$2:$1};
-    $fullname =~ s{^(.*?):(.*?):releasedef:(.*?)$}
-	          {$1$self->{delimiter}$2:releasedef:$3};
     return $fullname;
 }
 
@@ -526,51 +520,68 @@ sub _fullname2objectname
 # and %displayname) and the table in attribute "pseudo_attrs" 
 # of base-1:model:base.
 
-# NOTE: out methods are never called with $_[1] undef
+# rewrite rules for complex pseudo attributes
+# - key is the name of the pseudo attribute
+# - value is a hash consisting of:
+#   format: the string to use in a ccm format option to get the raw value
+#   rewrite: a sub that will be called with two arguments: a VCS::Synergy
+#     session and the raw value; it must return the converted value
+#     NOTE: the raw value will always be defined, because undef ("<void>")
+#       raw values are automatically passed thru
+#   row_object_ok: whether this pseudo attribute allowed when the final
+#     answer is in terms of VCS::CMSynergy::Objects (e.g. query_object())
 my %_rewrite_rule = 
 (
     objectname => 
     {
-	in		=> "%objectname",
-	out		=> sub { $_[0]->_fullname2objectname($_[1]); },
+	format		=> "%objectname",
+	rewrite		=> sub { my ($self, $value) = @_;
+	                         $self->_fullname2objectname($value); },
 	row_object_ok	=> 1,
     },
     object => 
     {
-	in		=> "%objectname",
-	out		=> sub { $_[0]->object($_[0]->_fullname2objectname($_[1])); },
+	format		=> "%objectname",
+	rewrite		=> sub { my ($self, $value) = @_;
+				 $self->object($self->_fullname2objectname($value)); },
 	row_object_ok	=> 1,
     },
     task_objects => 
     {
-	in		=> "%task",
-	out		=> sub { [ map { $_[0]->task_object($_) } split(/,/, $_[1]) ]; },
+	format		=> "%task",
+	rewrite		=> sub { my ($self, $value) = @_;
+				 [ map { $self->task_object($_) } 
+				       split(/,/, $value) ]; },
 	row_object_ok	=> 0,
     },
     cr_objects =>
     {
-	in		=> "%change_request",
-	out		=> sub { [ map { $_[0]->cr_object($_) } split(/,/, $_[1]) ]; },
+	format		=> "%change_request",
+	rewrite		=> sub { my ($self, $value) = @_;
+				 [ map { $self->cr_object($_) } 
+				       split(/,/, $value) ]; },
 	row_object_ok	=> 0,
     },
     baseline_project =>
     {
-	in		=> "%baseline",
-	out		=> sub { $_[0]->project_object($_[0]); },
+	format		=> "%baseline",
+	rewrite		=> sub { my ($self, $value) = @_;
+				 $self->project_object($value); },
 	row_object_ok	=> 0,
     },
     baseline_object =>
     {
-	in		=> "%in_baseline",
-	out		=> sub { $_[0]->baseline_object($_[1]); },
+	format		=> "%in_baseline",
+	rewrite		=> sub { my ($self, $value) = @_;
+				 $self->baseline_object($value); },
 	row_object_ok	=> 0,
     },
 );
 
 
 # helper (not a method): build "want" array from keyword list (common case)
-# NOTE: if $want_row_object is true, key "object" will be automatically
-#       added to the returned hash
+# NOTE: if $want_row_object is true, the keyword "object" will be 
+#   automatically added to the returned hash
 sub _want
 {
     my ($want_row_object, $keywords) = @_;
@@ -584,7 +595,7 @@ sub _want
 	{
 	    croak(__PACKAGE__.qq[::_want: keyword "$_" not allowed when ROW_OBJECT wanted]) 
 		if $want_row_object && !$rule->{row_object_ok};
-	    $want{$_} = $rule->{in};
+	    $want{$_} = $rule->{format};
 	}
     }
 
@@ -598,16 +609,17 @@ sub _parse_query_result
     my %row;
     
     # strip trailing newline (for consistency with get_attribute()),
-    # translate "<void>" to undef and fill into correct slots;
-    # @$cols are in the same order as keys %$want
+    # translate "<void>" to undef and fill into correct slots
+    # NOTE: per construction, @$cols are in the same order as keys %$want
     @row{keys %$want} = map { s/\n\z//; /^<void>$/ ? undef : $_ } @$cols;
     
     # handle special keywords
     foreach (keys %$want)
     {
-	if ($_rewrite_rule{$_} && defined $row{$_})
+	next unless defined $row{$_};
+	if (my $rule = $_rewrite_rule{$_})
 	{
-	    $row{$_} = $_rewrite_rule{$_}->{out}->($self, $row{$_});
+	    $row{$_} = $rule->{rewrite}->($self, $row{$_});
 	}
     }
 
